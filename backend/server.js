e161fc3a77ea1fafe5cb2f77737377ea2d6c9495
elix-Nepal <smithbruv169@gmail.com>
2026-06-04 17:48:03 +0545

    remove rate limit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const rateLimit = require('express-rate-limit');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
